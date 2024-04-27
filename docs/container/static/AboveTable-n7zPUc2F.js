59e28076ca68f73bd65ee78c1693459a5c0a8012
William <test@test.com>
2024-04-28 00:27:49 +0800

    Add files

---
+++ docs/container/static/AboveTable-n7zPUc2F.js
@@ -0,0 +1,19 @@
+import{u as a}from"./index-tEWTe_cM.js";import{aR as d,aS as o}from"./Learn-ZI4QJ8Ms.js";const s=a.div`
+  border: 1px solid ${({theme:r})=>r.systemColors.grey200};
+  border-bottom: none;
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+
+  padding: ${({theme:{spacing:{sp16:r,sp20:e,sp24:t}}})=>`${r} ${t} ${e}`};
+
+  & + div ${d}, & + div ${o} {
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+
+  & + ${o} {
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+`;export{s as A};
+//# sourceMappingURL=AboveTable-n7zPUc2F.js.map
---
+++ docs/container/static/AboveTableSkeleton-Lgx9__nY.js
@@ -0,0 +1,37 @@
+import{j as i,cA as t,u as s,I as l}from"./index-tEWTe_cM.js";const g=()=>{const e={size:"small",bottom:12},d={size:"medium",width:180,bottom:0};return i.jsxs(o,{children:[i.jsx(c,{children:i.jsxs(x,{children:[i.jsx(n,{children:i.jsx(t,{builder:[{...d}]})}),i.jsxs(m,{children:[i.jsx(t,{builder:[{size:"medium",bottom:32}]}),i.jsxs(u,{children:[i.jsx(t,{builder:[{size:"large",bottom:0,width:140}]}),i.jsx(r,{children:i.jsx(t,{builder:[{size:"large",bottom:0,width:330}]})})]})]})]})}),i.jsx(a,{children:i.jsxs(h,{children:[i.jsx(n,{children:i.jsx(t,{builder:[{...d}]})}),i.jsx(t,{builder:[{...e,width:190},{...e,width:140},{...e,width:165},{...e,width:190}]})]})}),i.jsx(p,{children:i.jsxs(j,{children:[i.jsxs(n,{children:[i.jsx(t,{builder:[{...d}]}),i.jsx(r,{children:i.jsx(t,{builder:[{size:"large",width:71}]})})]}),i.jsx(t,{builder:[{...e,width:355},{...e,width:400},{...e,width:340},{...e,width:382}]})]})})]})},o=s.div`
+  display: grid;
+  flex-direction: column;
+  grid-template-columns: 2fr 1fr;
+  width: 100%;
+
+  ${l.laptop} {
+    grid-template-columns: 3fr 1fr 2fr;
+  }
+`,c=s.div``,a=s.div`
+  border-left: 1px solid #d9e0e8;
+  padding-left: ${({theme:e})=>e.spacing.sp24};
+  margin-left: ${({theme:e})=>e.spacing.sp24};
+`,p=s.div`
+  border-left: 1px solid #d9e0e8;
+  padding-left: ${({theme:e})=>e.spacing.sp24};
+  margin-left: ${({theme:e})=>e.spacing.sp24};
+  display: none;
+
+  ${l.laptop} {
+    display: block;
+  }
+`,x=s.div``,h=s.div``,j=s.div``,n=s.div`
+  margin-bottom: ${({theme:e})=>e.spacing.sp16};
+  display: flex;
+  justify-content: space-between;
+  height: ${({theme:e})=>e.spacing.sp32};
+  align-items: center;
+`,r=s.div`
+  margin-left: ${({theme:e})=>e.spacing.sp12};
+`,m=s.div`
+  display: grid;
+`,u=s.div`
+  display: flex;
+  justify-content: space-between;
+`;export{g as A};
+//# sourceMappingURL=AboveTableSkeleton-Lgx9__nY.js.map
